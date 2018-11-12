7ec321ca43276035b862547eba68520d26cd87de
Andrew Piper <andrew@threax.com>
2018-11-12 08:38:51 -0500

    Added model schemas.

---
+++ Budget/ModelSchemas/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budget.ModelSchemas
+{
+    public class Category
+    {
+        public String Name { get; set; }
+    }
+}
