7dda1a341990f00a1aecdde6822682a1f5aca690
Andrew Piper <andrew@threax.com>
2021-01-29 08:58:14 -0500

    Removed docker cert manager.

--- Budget/Startup.cs
+++ Budget/Startup.cs
@@ -68,8 +68,6 @@ namespace Budget
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Threax.AspNetCore.Docker.Certs.CertManager.LoadTrustedRoots(o => Configuration.Bind("CertManager", o));
-
             //Add the client side configuration object
             services.AddClientConfig(clientConfig, o =>
             {
